004f135409eca7a44e70efb75975e7a767487cd7
bkalish <bkalish@forbeslibrary.org>
2013-04-03 14:29:25 -0400

    404 page can now be customized from the configuration form

--- error/404.php
+++ error/404.php
@@ -2,6 +2,9 @@
 header("HTTP/1.0 404 Not Found");
 head(); ?>
 <h1>404</h1>
-<p>The URL <?php echo $_SERVER['REQUEST_URI']; ?> was not found on this server.</p>
- 
-<?php foot(); ?>
+<p>The URL <code><?php echo $_SERVER['REQUEST_URI']; ?></code> was not found on this server.</p>
+<?php
+if (get_theme_option('404_help_text')) {
+  echo '<p>' . get_theme_option('404_help_text') . '</p>';
+}
+foot();
