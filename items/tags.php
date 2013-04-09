a8e1231bb17f37c8e2034377ce338bf35c5ac9aa
bkalish <bkalish@forbeslibrary.org>
2013-04-08 20:22:57 -0400

    completed tag browse enhancement (issue #35)

--- items/tags.php
+++ items/tags.php
@@ -6,6 +6,14 @@ head(array('title'=>$pageTitle,'bodyid'=>'items','bodyclass'=>'tags'));
 <?php common('browse-navigation', array('pageTitle'=>$pageTitle), 'items')?>
 <?php
 $tags = get_tags(array('sort' => 'alpha'), null);
+// Remove tags to unreachable items (such as non-public items for a non-logged in user)
+foreach ($tags as $i => $tag) {
+    $tag_name = $tag['name'];
+    $items = get_items(array('tags' => $tag_name));
+    if (count($items)==0) {
+        unset($tags[$i]);
+    }
+}
 echo tag_cloud($tags,uri('items/browse'));
 ?>
 
