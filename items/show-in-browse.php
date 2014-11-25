26aaeecadf3fd457b8351e2dfa02257e253695bf
Benjamin Kalish <bkalish@forbeslibrary.org>
2014-11-24 20:34:43 -0500

    added comments in items/show-in-browse for greater legibility

--- items/show-in-browse.php
+++ items/show-in-browse.php
@@ -1,26 +1,33 @@
-<li class="items-show-in-browse">
+<section class="items-show-in-browse">
+	<!-- item section header -->
 	<h2><?php
 		$title = metadata($item, array('Dublin Core', 'Title'), array('snippet'=>50));
 		$id = metadata($item, array('Dublin Core', 'Identifier'));
 		$line = $title ? $id.': '.$title : $id; 
 		echo link_to_item($line, array('class'=>'permalink'));
 	?></h2>
-	<figure>
-    	<?php $thumbnail = (metadata($item, 'has thumbnail') ? item_image('thumbnail', array('class'=>'thumbnail')) :  '<img src="'.img('image-not-available.png').'">' ); ?>
-		<?php echo link_to_item($thumbnail); ?>
-	</figure>
+	
+	<!-- item section thumbnail -->
+  <?php $thumbnail = (metadata($item, 'has thumbnail') ? item_image('thumbnail', array('class'=>'thumbnail')) :  '<img src="'.img('image-not-available.png').'">' ); ?>
+	<?php echo link_to_item($thumbnail); ?>
+	
+	<!-- item section metadata -->
 	<div class="items-show-in-browse-details">
-	    <?php if ($format = metadata($item, array('Dublin Core', 'Format'))) {
-	        echo __('Format: %s.', $format);
-	    } ?>
-	    <?php if ($creator = metadata($item, array('Dublin Core', 'Creator'))) {
-	        echo __('Creator: %s.', $creator);
-	    } ?>
+		<?php if ($format = metadata($item, array('Dublin Core', 'Format'))) {
+				echo __('Format: %s.', $format);
+		} ?>
+		<?php if ($creator = metadata($item, array('Dublin Core', 'Creator'))) {
+				echo __('Creator: %s.', $creator);
+		} ?>
 
-        <div class="items-show-in-browse-description">
-            <?php echo forbes_theme_snippet_with_new_lines(metadata($item, array('Dublin Core', 'Description')),0,200); ?>
-        </div>
-		<?php echo fire_plugin_hook('append_to_items_browse_each'); ?>
-		<?php echo link_to_item(__('More information'), array('class'=>'items-show-in-browse-details')); ?>
+		<div class="items-show-in-browse-description">
+				<?php echo forbes_theme_snippet_with_new_lines(metadata($item, array('Dublin Core', 'Description')),0,200); ?>
+		</div>
 	</div>
-</li>
+	
+	<!-- plugin hook append_to_items_browse_each -->
+	<?php echo fire_plugin_hook('append_to_items_browse_each'); ?>
+	
+	<!-- link to item -->
+	<?php echo link_to_item(__('More information'), array('class'=>'items-show-in-browse-details')); ?>
+</section>
