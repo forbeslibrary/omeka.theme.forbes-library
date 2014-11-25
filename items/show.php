17c0ba3734a76281c7104a0ef71350257751daf5
Benjamin Kalish <bkalish@gmail.com>
2014-11-24 23:31:03 -0500

    further migration in items/show

--- items/show.php
+++ items/show.php
@@ -1,10 +1,15 @@
-<?php head(array('title' => item('Dublin Core', 'Title'), 'bodyid'=>'items','bodyclass' => 'show')); ?>
+<?php
+$title       = metadata($item, array('Dublin Core', 'Title'));
+$identifier  = metadata($item, array('Dublin Core', 'Identifier'));
+$description = metadata($item, array('Dublin Core', 'Description'));
+?>
+<?php echo head(array('title' => $title, 'bodyid'=>'items','bodyclass' => 'show')); ?>
 <div id="items-show-id">
     <?php
-    if (item('Dublin Core', 'Title')) {
-        echo item('Dublin Core', 'Title'), '. ';
+    if ($title) {
+        echo $title, '. ';
     } ?>
-    Image ID: <?php echo item('Dublin Core', 'Identifier');
+    Image ID: <?php echo $identifier;
     if (function_exists('forbes_purchase_form_public_append_to_items_show')) {
 	    echo ' (';
 	    forbes_purchase_form_public_append_to_items_show();
@@ -13,16 +18,16 @@
 </div>
 <div id="files-container">
 	<!-- The following returns all of the files associated with an item. -->
-	<div><?php echo display_files_for_item(array('imageSize' => 'fullsize')); ?></div>
+	<div><?php echo files_for_item(array('imageSize' => 'fullsize')); ?></div>
 </div>
 <div id="items-show-description-box">
 	<!-- If the item belongs to a collection, the following creates a link to that collection. -->
-	<?php if (item_belongs_to_collection()): ?>
+	<?php if ($item->collection): ?>
 	<div id="items-show-collection">
 		Part of the online collection <?php echo link_to_collection_for_item(); ?>
 	</div>
 	<?php endif; ?>
-	<div id="items-show-description"><?php echo item('Dublin Core', 'Description'); ?></div>
+	<div id="items-show-description"><?php echo $description; ?></div>
 </div>
 <div id="item-metadata-wrapper">
 	<h2 id="item-metadata-button" class="_toggle_button">Image Details</h2>
@@ -53,4 +58,4 @@ if ($pluginAdditions) {
 	</ul>
 </div>
 <?php endif;
-foot();
+echo foot();
