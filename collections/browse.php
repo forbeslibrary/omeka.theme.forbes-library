0396371b552f55abfc817af28c0f752fd47203a1
Benjamin Kalish <bkalish@gmail.com>
2014-12-03 10:18:03 -0500

    finished migrating collections browse to Omeka 2.x

--- collections/browse.php
+++ collections/browse.php
@@ -1,6 +1,6 @@
 <?php
 $pageTitle = __('Collections');
-head(array('title'=>$pageTitle,'bodyid'=>'collections','bodyclass' => 'browse'));
+echo head(array('title'=>$pageTitle,'bodyid'=>'collections','bodyclass' => 'browse'));
 ?>
 <h1><?php echo $pageTitle; ?></h1>
 
@@ -14,33 +14,23 @@ set_loop_records('collections', get_records('collection', array ('sort_field'=>'
 <?php foreach (loop('collections') as $collection): ?>
     <?php set_current_record('collection', $collection); ?>
     <li class="collections-browse-collection-entry">
-        <?php echo link_to_collection('<h2>'.metadata('collection', array('Dublin Core', 'Title')).'</h2>'); ?>
-        <?php if ($thumbnail = forbes_theme_collection_thumbnail()): ?> 
-            <figure><?php echo link_to_collection($thumbnail);?></figure>
-        <?php endif; ?>
-        </figure>
-        
-        <?php if ($description = forbes_theme_snippet_with_new_lines(get_current_collection()->description, 0, 400)): ?>
-        <div>
-            <h3><?php echo __('Description'); ?></h3>
-            <div><?php echo nls2p($description); ?></div>
-        </div>
-        <?php endif; ?>
-    
-        <?php if(collection_has_collectors()): ?>
-        <div>
-            <h3><?php echo __('Collector(s)'); ?></h3>
-            <div class="element-text">
-                <p><?php echo collection('Collectors', array('delimiter'=>'. ')); ?>.</p>
-            </div>
-        </div>
-        <?php endif; ?>
-    
-        <?php echo plugin_append_to_collections_browse_each(); ?>
-        <?php echo link_to_collection(__('More on this collection.')); ?>
+      <?php echo link_to_collection('<h2>'.metadata('collection', array('Dublin Core', 'Title')).'</h2>'); ?>
+      <?php echo text_to_paragraphs(metadata('collection', array('Dublin Core', 'Description'))); ?>
+  
+      <?php if(metadata('collection', array('Dublin Core', 'Contributor'))): ?>
+      <div>
+          <h3><?php echo __('Collector(s)'); ?></h3>
+          <div class="element-text">
+              <p><?php echo metadata('collection', array('Dublin Core', 'Contributor')); ?>.</p>
+          </div>
+      </div>
+      <?php endif; ?>
+  
+      <?php echo fire_plugin_hook('append_to_collections_browse_each'); ?>
+      <?php echo link_to_collection(__('More on this collection.')); ?>
     </li>
 <?php endforeach; ?>
 </ul>
 
-<?php echo plugin_append_to_collections_browse(); ?>
-<?php foot(); ?>
+<?php echo fire_plugin_hook('append_to_collections_browse'); ?>
+<?php echo foot(); ?>
