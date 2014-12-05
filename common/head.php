086926fef4115b5c5130f86802207b7399edd483
= <=>
2014-12-05 15:36:24 -0500

    added suport for banner background image

--- common/head.php
+++ common/head.php
@@ -34,6 +34,12 @@
 	
 	<!-- stylesheets -->
 	<?php
+	$banner_image = get_theme_option('banner');
+	if ($banner_image) {
+	  $storage = Zend_Registry::get('storage');
+    $banner_background_url = $storage->getUri($storage->getPathByType($banner_image, 'theme_uploads'));
+	  queue_css_string("#banner { background: url('$banner_background_url') center; background-size: cover; }");
+	}
 	queue_css_url('//fonts.googleapis.com/css?family=Lato:300,400,700,900,400italic,700italic');
 	queue_css_file('style');
 	echo head_css();
