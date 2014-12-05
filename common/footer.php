4fc0180a21756ff74c76d25882e9a7d6cb6f72c1
= <=>
2014-12-05 12:03:34 -0500

    fixed bug in displaying custom footer text from user options

--- common/footer.php
+++ common/footer.php
@@ -7,7 +7,7 @@ $forbesThemeSession = new Zend_Session_Namespace('forbes_theme');
 
 		<div id="footer-text">
 			<?php echo get_theme_option('Footer Text'); ?>
-			<?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = settings('copyright')): ?>
+			<?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = get_option('copyright')): ?>
 				<p><?php echo $copyright; ?></p>
 			<?php endif; ?>
 			<p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
