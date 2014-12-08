fe85e25cd48f8e76c97e111c0ec985db64b1003a
Benjamin Kalish <bkalish@gmail.com>
2014-12-08 18:38:20 -0500

    now truncated desriptions in the featured content will have html tags closed properly

--- custom.php
+++ custom.php
@@ -323,11 +323,47 @@ function fobres_theme_link_to_items_in_collection(
  * Truncates text at the wordpress style <!-- more --> tag.
  */
 function forbes_theme_summary($html) {
-	$pattern = '/^(.*)<!--\s*more/is';
-	preg_match($pattern, $html, $matches);
-	if (isset ($matches[1])) {
-	  return $matches[1];
-	} else {
-	  return $html;
-	}
+  $pattern = '/^(.*)<!--\s*more/is';
+  preg_match($pattern, $html, $matches);
+  if (isset ($matches[1])) {
+    return forbes_theme_closetags($matches[1]);
+  } else {
+    return $html;
+  }
+}
+
+/**
+ * Close open html tags.
+ *
+ * Note that this is not a general HTML cleanup tool and will not help if the tags are improperly nested.
+ * It's intended function is only to close tags when a fragment of valid HTML must be inserted into markup.
+ */
+ // close opened html tags
+function forbes_theme_closetags($html) {
+  #put all opened tags into an array
+  preg_match_all ( "#<([a-z]+)( .*)?(?!/)>#iU", $html, $result );
+  $openedtags = $result[1];
+  #put all closed tags into an array
+  preg_match_all ( "#</([a-z]+)>#iU", $html, $result );
+  $closedtags = $result[1];
+  $len_opened = count ( $openedtags );
+  # all tags are closed
+  if( count ( $closedtags ) == $len_opened )
+  {
+  return $html;
+  }
+  $openedtags = array_reverse ( $openedtags );
+  # close tags
+  for( $i = 0; $i < $len_opened; $i++ )
+  {
+      if ( !in_array ( $openedtags[$i], $closedtags ) )
+      {
+      $html .= "</" . $openedtags[$i] . ">";
+      }
+      else
+      {
+      unset ( $closedtags[array_search ( $openedtags[$i], $closedtags)] );
+      }
+  }
+  return $html;
 }
