dfa9f3ee799f352fa89902e0f2546f8e20dbabb4
Benjamin Kalish <bkalish@gmail.com>
2014-12-09 14:24:29 -0500

    improvements to site style

--- common/header.php
+++ common/header.php
@@ -10,42 +10,43 @@ $forbesThemeSession = new Zend_Session_Namespace('forbes_theme');
   <!-- plugin hook 'public_body' -->
   <?php fire_plugin_hook('public_body', array('view'=>$this)); ?>
   <div id="wrapper">
-	<header id="page-header">
-    <!-- plugin hook 'public_header' -->
-	  <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
+    <header id="page-header">
+      <!-- plugin hook 'public_header' -->
+      <?php fire_plugin_hook('public_header', array('view'=>$this)); ?>
 
-		<!-- front matter (includes site title, quick links, and search -->
+      <!-- front matter (includes site title, quick links, and search -->
+      <div id="banner">
 
-		<div id="banner">
+      <!-- custom navigation links as defined in the theme configuration -->
+      <div id="header-buttons">
+        <?php echo forbes_theme_public_header_nav(); ?>
+      </div>
+
+      <!-- simple search form -->
+      <form id="simple-search" action="<?php echo url('items/browse'); ?>" method="get">
+        <input type="search" name="search" id="search" value="" class="textinput">
+        <input type="submit" name="submit_search" id="submit_search" value="Search">
+      </form>
+
+      <!-- site title and logo -->
       <h1 id="site-title">
-			  <?php
-				if (theme_logo()) {
-				  echo link_to_home_page(theme_logo()); 
-				} else {
-				  echo link_to_home_page();
-				}
-				?>
-			</h1>
+        <?php
+        if (theme_logo()) {
+          echo link_to_home_page(theme_logo()); 
+        } else {
+          echo link_to_home_page();
+        }
+        ?>
+      </h1>
     </div>
 
-    <!-- custom navigation links as defined in the theme configuration -->
-		<div id="header-buttons">
-		  <?php echo forbes_theme_public_header_nav(); ?>
-		</div>
+    <!-- menu bar -->
+    <nav id="top-level-nav" class="menu_bar">
+      <h2 class="navigation-label"><a href="<?php echo url('?nav=True');?>"><?php echo __('Navigation'); ?></a></h2>
+      <span class="nav-jump-to-content"><a href="#content" tabindex="0"><?php echo __('Skip to content') ?></a></span>
+      <?php echo public_nav_main(); ?>
+    </nav>
+  </header>
 
-		<!-- simple search form -->
-		<form id="simple-search" action="<?php echo url('items/browse'); ?>" method="get">
-			<input type="search" name="search" id="search" value="" class="textinput">
-			<input type="submit" name="submit_search" id="submit_search" value="Search">
-		</form>
-
-		<!-- menu bar -->
-		<nav id="top-level-nav" class="menu_bar">
-			<h2 class="navigation-label"><a href="<?php echo url('?nav=True');?>"><?php echo __('Navigation'); ?></a></h2>
-			<span class="nav-jump-to-content"><a href="#content" tabindex="0"><?php echo __('Skip to content') ?></a></span>
-			<?php echo public_nav_main(); ?>
-		</nav>
-	</header>
-
-	<div id="content">
-		<?php fire_plugin_hook('public_content', array('view'=>$this));
+  <div id="content">
+    <?php fire_plugin_hook('public_content', array('view'=>$this));
