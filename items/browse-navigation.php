c461721fef98a401fa71d93874a3f2e9507d9c1f
Benjamin Kalish <bkalish@gmail.com>
2014-12-11 19:42:17 -0500

    removed special item browse navigation

--- items/browse-navigation.php
+++
@@ -1,9 +0,0 @@
-<nav id="items-browse-nav">
-<?php
-$navArray = array(
-    array( 'label' => __('Browse All'), 'uri' => url('items')), 
-    array( 'label' => __('View Tags'), 'uri' => url('items/tags'))
-);
-echo nav($navArray);
-?>
-</nav>
