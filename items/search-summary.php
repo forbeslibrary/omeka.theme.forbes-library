db5be5e1539c547f63555b205e055afb87ad79c1
bkalish <bkalish@gmail.com>
2015-04-17 10:24:40 -0400

    fixes #11. improvements and bug fixes in search summaries.

--- items/search-summary.php
+++ items/search-summary.php
@@ -1,67 +1,77 @@
 <?php
 if (isset($_GET['search']) && $_GET['search']!='') {
-	$limits[] = __(
-    		'with at least one of the words in the query "%s"',
-    		html_escape($_GET['search'])
-    		);
-
+  $limits[] = __(
+    'with at least one of the words in the query "%s"',
+    html_escape($_GET['search'])
+  );
 }
 if (isset($_GET['advanced'])) {
     foreach ($_GET['advanced'] as $search_line) {
         $element = @get_db()->getTable('Element')->find($search_line['element_id'])->name;
         $type = $search_line['type'];
-        $terms = $search_line['terms'];
-        if ($element=='' or $type=='' or $terms=='') {
+        if (isset($search_line['terms'])) {
+          $terms = $search_line['terms'];
+        } else {
+          $terms = '';
+        }
+        if ($element=='' or $type=='') {
             continue;
         }
-        $limits[] = html_escape(__('where the %1$s field %2$s \'%3$s\'', $element, $type, $terms));
-    } 
+        if ($terms=='') {
+          $limits[] = html_escape(__('where the %1$s field %2$s', $element, $type));
+        } else {
+          $limits[] = html_escape(__('where the %1$s field %2$s \'%3$s\'', $element, $type, $terms));
+        }
+    }
 }
 if (isset($_GET['range']) && $_GET['range']!='') {
-    $range = $_GET['range'];
-    if (is_numeric($range)) {
-        $limits[] = __('with id %s.', html_escape($_GET['range']));
-    } else {
-        $limits[] = __('with ids %s.', html_escape($_GET['range']));
-    } 
+  $range = $_GET['range'];
+  if (is_numeric($range)) {
+    $limits[] = __('with id %s', html_escape($_GET['range']));
+  } else {
+    $limits[] = __('with ids %s', html_escape($_GET['range']));
+  }
 }
 if (isset($_GET['collection']) && $_GET['collection']!='') {
-	$collection_id = $_GET['collection'];
-	if (is_numeric($collection_id) and ($collection = get_record_by_id('collection', $collection_id))) {
-		set_current_record('collection', $collection);
-	    $limits[] = __(
-	    	'in the collection <a href="%1$s">%2$s</a>',
-	    	url('collections/show/'.$collection_id),
-	    	metadata('collection', array('Dublin Core', 'Title'))
-	    	);
-	} elseif ($collection_id !='') {
-	    $limits[] = __('in the collection with id %s (collection does not exist).', html_escape($collection_id));  
-	}
+  $collection_id = $_GET['collection'];
+  if (is_numeric($collection_id) and ($collection = get_record_by_id('collection', $collection_id))) {
+    set_current_record('collection', $collection);
+      $limits[] = __(
+        'in the collection <a href="%1$s">%2$s</a>',
+        url('collections/show/'.$collection_id),
+        metadata('collection', array('Dublin Core', 'Title'))
+        );
+  } elseif ($collection_id !='') {
+      $limits[] = __('in the collection with id %s (collection does not exist)', html_escape($collection_id));
+  }
 }
 if (isset($_GET['type']) && $_GET['type']!='') {
         $type_id = $_GET['type'];
-	$table = get_db()->getTable('ItemType');
-	$type_name = $table->find($type_id)->name;
-	if ($type_name) {
-            $limits[] = __('with type %s.', html_escape($type_name));
+  $table = get_db()->getTable('ItemType');
+  $type_name = $table->find($type_id)->name;
+  if ($type_name) {
+            $limits[] = __('where the type is %s', html_escape($type_name));
         } else {
-            $limits[] = __('which have the type with the id %s (type does not exist).', html_escape($type_id));
+            $limits[] = __('where the type has the id %s (type does not exist)', html_escape($type_id));
         }
 }
 if (isset($_GET['tags']) && $_GET['tags']!='') {
-	$limits[] = __(
-		'with the tags "%s"',
-		$_GET['tags']
-		);	
+  $delimiter = get_option('tag_delimiter');
+  $tags = explode($delimiter, $_GET['tags']);
+  $limits[] = __(
+    'with the %1$s "%2$s"',
+		count($tags) ? __('tag') : __('tags'),
+    implode($delimiter, $tags)
+    );
 }
 if (isset($_GET['user']) && $_GET['user']!='') {
     $user_id = $_GET['user'];
     $table = get_db()->getTable('User');
     $username = $table->find($user_id)->username;
     if ($username) {
-        $limits[] = __('created by %s.', html_escape($username));
+        $limits[] = __('created by %s', html_escape($username));
     } else {
-        $limits[] = __('created by user with the id %s (user does not exist).', html_escape($user_id));
+        $limits[] = __('created by user with the id %s (user does not exist)', html_escape($user_id));
     }
 }
 if (isset($_GET['public']) && $_GET['public']) {
@@ -79,10 +89,10 @@ if (isset($_GET['featured']) && $_GET['featured']) {
     }
 }
 if ($tag = Zend_Controller_Front::getInstance()->getRequest()->getParam('tag')) {
-	$limits[] = __(
-		'with the tag "%s"',
-		$tag
-		);	
+  $limits[] = __(
+    'with the tag "%s"',
+    $tag
+    );
 }
 if (isset($limits)): ?>
 <?php
