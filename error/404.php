2cc53c178e7f49b685e98f2981efb75b1049921f
bkalish <bkalish@gmail.com>
2015-05-12 16:35:22 -0400

    404 page now uses custom html title

--- error/404.php
+++ error/404.php
@@ -1,7 +1,15 @@
 <?php
 header("HTTP/1.0 404 Not Found");
-echo head(); ?>
-<h1>404</h1>
+$pageTitle = __('404 Not Found');
+echo head(
+  array(
+    'title' => $pageTitle,
+    'bodyid' => '404',
+    'bodyclass' => 'error 404'
+  )
+);
+?>
+<h2><?php echo $pageTitle; ?></h2>
 <p>The URL <code><?php echo $_SERVER['REQUEST_URI']; ?></code> was not found on this server.</p>
 <?php
 if (get_theme_option('404_help_text')) {
