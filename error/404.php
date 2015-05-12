dcaaaff4837fe66f67901257b19d9f9875e1a893
bkalish <bkalish@gmail.com>
2015-05-12 17:04:56 -0400

    increased legibility of 404.php

--- error/404.php
+++ error/404.php
@@ -1,18 +1,31 @@
 <?php
+// Output HTTP header
 header("HTTP/1.0 404 Not Found");
+
+// Set variables for this template
+$errorMessage = __(
+  'The URL %s was not found on this server.',
+  $_SERVER['REQUEST_URI']
+  );
 $pageTitle = __('404 Not Found');
-echo head(
-  array(
-    'title' => $pageTitle,
-    'bodyid' => '404',
-    'bodyclass' => 'error 404'
-  )
-);
+$headVars = array(
+  'title' => $pageTitle,
+  'bodyid' => '404',
+  'bodyclass' => 'error 404'
+  );
+
+// Content begins here
 ?>
-<h2><?php echo $pageTitle; ?></h2>
-<p>The URL <code><?php echo $_SERVER['REQUEST_URI']; ?></code> was not found on this server.</p>
-<?php
-if (get_theme_option('404_help_text')) {
-  echo '<p>' . get_theme_option('404_help_text') . '</p>';
-}
-echo foot();
+<?php echo head($headVars); ?>
+<h2>
+  <?php echo $pageTitle; ?>
+</h2>
+<p>
+  <?php echo $errorMessage; ?>
+</p>
+<?php if (get_theme_option('404_help_text')): ?>
+  <p>
+    <?php echo get_theme_option('404_help_text'); ?>
+  </p>
+<?php endif ?>
+<?php echo foot();
