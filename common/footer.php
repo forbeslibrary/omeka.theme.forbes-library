3e03fd2d2280b732d6ad54b230db9b9e07c7709c
bkalish <bkalish@gmail.com>
2015-05-16 10:45:07 -0400

    improved legibility of common/footer.php

--- common/footer.php
+++ common/footer.php
@@ -1,20 +1,35 @@
 <?php
-$forbesThemeSession = new Zend_Session_Namespace('forbes_theme');
+
+/**
+ * header.php template for the forbes-library Omeka theme
+ *
+ * This partial template looks for the following variables:
+ *  - $title
+ *  - $bodyid
+ *  - $bodyclass
+ */
+
+// == Set variables for this template =========================================
+$copyright = null;
+if (get_theme_option('Display Footer Copyright') == 1) {
+	$copyright = get_option('copyright');
+}
+
+// == Content begins here =====================================================
 ?>
 	</div><!-- end content -->
-
-	<footer>
-
-		<div id="footer-text">
-			<?php echo get_theme_option('Footer Text'); ?>
-			<?php if ((get_theme_option('Display Footer Copyright') == 1) && $copyright = get_option('copyright')): ?>
-				<p><?php echo $copyright; ?></p>
-			<?php endif; ?>
-			<p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
-		</div>
-
-		<?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
-	</footer>
+		<footer>
+			<div id="footer-text">
+				<?php echo get_theme_option('Footer Text'); ?>
+				<?php if ($copyright): ?>
+					<p>
+						<?php echo $copyright; ?>
+					</p>
+				<?php endif; ?>
+				<p><?php echo __('Proudly powered by <a href="http://omeka.org">Omeka</a>.'); ?></p>
+			</div>
+			<?php fire_plugin_hook('public_footer', array('view'=>$this)); ?>
+		</footer>
   </div>
 </body>
 </html>
