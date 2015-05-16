7e96ab1cb1aa360c1ed728ce09a488f2db602643
bkalish <bkalish@gmail.com>
2015-05-16 09:35:28 -0400

    more legibility improvements to 404.php

--- error/404.php
+++ error/404.php
@@ -1,31 +1,38 @@
 <?php
-// Output HTTP header
+
+/**
+ * error/404.php template for the forbes-library Omeka theme
+ *
+ * This template may be called without arguments.
+ */
+
+// == Output HTTP header ======================================================
 header("HTTP/1.0 404 Not Found");
 
-// Set variables for this template
+// == Set variables for this template =========================================
 $errorMessage = __(
-  'The URL %s was not found on this server.',
-  $_SERVER['REQUEST_URI']
-  );
+	'The URL %s was not found on this server.',
+	$_SERVER['REQUEST_URI']
+	);
 $pageTitle = __('404 Not Found');
 $headVars = array(
-  'title' => $pageTitle,
-  'bodyid' => '404',
-  'bodyclass' => 'error 404'
-  );
+	'title' => $pageTitle,
+	'bodyid' => '404',
+	'bodyclass' => 'error 404'
+	);
 
-// Content begins here
+// == Content begins here =====================================================
 ?>
 <?php echo head($headVars); ?>
 <h2>
-  <?php echo $pageTitle; ?>
+	<?php echo $pageTitle; ?>
 </h2>
 <p>
-  <?php echo $errorMessage; ?>
+	<?php echo $errorMessage; ?>
 </p>
 <?php if (get_theme_option('404_help_text')): ?>
-  <p>
-    <?php echo get_theme_option('404_help_text'); ?>
-  </p>
+	<p>
+		<?php echo get_theme_option('404_help_text'); ?>
+	</p>
 <?php endif ?>
 <?php echo foot();
