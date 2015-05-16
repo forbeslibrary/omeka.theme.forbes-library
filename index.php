92dde549114c316736efb3189e952ce2e91ab71a
bkalish <bkalish@gmail.com>
2015-05-16 10:07:18 -0400

    improved legibility of index.php

--- index.php
+++ index.php
@@ -1,14 +1,24 @@
+<?php
+
+/**
+ * index.php template for the forbes-library Omeka theme
+*/
+
+// == Content begins here =====================================================
+
 <?php echo head(array('bodyid'=>'home')); ?>
 
 <?php if (get_theme_option('Homepage Text')): ?>
-<div id="home-page-text"><?php echo get_theme_option('Homepage Text'); ?></div>
+  <div id="home-page-text">
+    <?php echo get_theme_option('Homepage Text'); ?>
+  </div>
 <?php endif; ?>
 
 <?php if (get_theme_option('Homepage Search')): ?>
-<form id="simple-search" action="<?php echo uri(array('controller'=>'items', 'action'=>'browse'))?>" method="get">
-    <input type="search" name="search" id="search" value="" class="textinput">
-    <input type="submit" name="submit_search" id="submit_search" value="Search">
-</form>
+  <form id="simple-search" action="<?php echo uri(array('controller'=>'items', 'action'=>'browse'))?>" method="get">
+      <input type="search" name="search" id="search" value="" class="textinput">
+      <input type="submit" name="submit_search" id="submit_search" value="Search">
+  </form>
 <?php endif; ?>
 
 <div id="featured-content" class="<?php echo ForbesTheme::featured_content_class(); ?>">
