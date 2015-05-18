62b5fed3127189c8ec0bfaeb9f2cbd90840f6fb3
bkalish <bkalish@gmail.com>
2015-05-18 17:34:04 -0400

    improved legibility of record-metadata.php

--- common/record-metadata.php
+++ common/record-metadata.php
@@ -1,17 +1,37 @@
-<?php foreach ($elementsForDisplay as $setName => $setElements): ?>
-  <table class="element-set">
-    <?php if ($showElementSetHeadings): ?>
-      <tr><th colspan="2" class="element-set-name"><?php echo html_escape(__($setName)); ?></th></tr>
-    <?php endif ?>
-    <?php foreach ($setElements as $elementName => $elementInfo): ?>
-    <tr>
-    <td class="element-name"><?php echo html_escape(__($elementName)); ?></td>
-    <td class="element-text">
-      <?php foreach ($elementInfo['texts'] as $text): ?>
-            <div class="element-text"><?php echo $text; ?></div>
-       <?php endforeach; ?>
-    </td>
-    </tr>
-  <?php endforeach; ?>
-  </table>
+<?php
+
+/**
+ * record-metadata.php template for the forbes-library Omeka theme
+ *
+ * Outputs a table of metadata. This method is used by all_element_texts().
+ *
+ * This partial template looks for the following variables:
+ *	- $elementsForDisplay
+ */
+
+// == Content begins here =====================================================
+foreach ($elementsForDisplay as $setName => $setElements): ?>
+	<table class="element-set">
+		<?php if ($showElementSetHeadings): ?>
+			<tr>
+				<th colspan="2" class="element-set-name">
+					<?php echo html_escape(__($setName)); ?>
+				</th>
+			</tr>
+		<?php endif ?>
+		<?php foreach ($setElements as $elementName => $elementInfo): ?>
+  		<tr>
+  			<td class="element-name">
+  				<?php echo html_escape(__($elementName)); ?>
+  			</td>
+  			<td class="element-text">
+  				<?php foreach ($elementInfo['texts'] as $text): ?>
+  					<div class="element-text">
+  						<?php echo $text; ?>
+  					</div>
+  				 <?php endforeach; ?>
+  			</td>
+  		</tr>
+	   <?php endforeach; ?>
+	</table>
 <?php endforeach;
